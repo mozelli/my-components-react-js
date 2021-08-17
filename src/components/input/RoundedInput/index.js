b317fcb6f4536ea36b469da8de4ea7af0ff70102
Joao Mozelli Neto <joaomozelli@hotmail.com>
2021-08-17 14:17:05 -0300

    feat: handle input state

--- src/components/input/RoundedInput/index.js
+++ src/components/input/RoundedInput/index.js
@@ -1,12 +1,25 @@
+import { useState } from "react";
+
 import "./styles.css";
 import EmailIcon from "../assets/email-icon.svg";
 
 function RoundedInput() {
+
+    const [ inputFocus, setInputFocus ] = useState(false);
+
+    function changeInputState() {
+        setInputFocus(!inputFocus);
+    }
+
     return (
         <div id="rounded-input">
             <label>Email</label>
             <div className="input-container">
-                <input type="text" />
+                <input 
+                    type="text"
+                    onFocus={ changeInputState }
+                    onBlur={ changeInputState } 
+                />
                 <img src={ EmailIcon } alt="Email" />
             </div>
         </div>
