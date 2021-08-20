b943ccc225256c3ac3ca1e9832ace79fa67ca1c3
Joao Mozelli Neto <joaomozelli@hotmail.com>
2021-08-20 16:19:19 -0300

    feat: sizing styles

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import RoundedInput from "./components/input/RoundedInput";
 
 import "./components/input/global.css";
 import EmailIcon from "./components/input/assets/email-icon.svg";
+import LockIcon from "./components/input/assets/lock-icon.svg";
 
 function App() {
   return (
     <div className="App">
-      <RoundedInput type="text" label="Usuário" icon={ EmailIcon } size="lg" />
-      <RoundedInput type="email" label="Email" icon={ EmailIcon } size="" />
-      <RoundedInput type="password" label="Senha" icon={ EmailIcon } size="sm" />
+      <RoundedInput type="text" label="Usuário" size="sm" />
+      <RoundedInput type="email" label="E-mail" icon={ EmailIcon } size="lg" />
+      <RoundedInput type="password" label="Senha" icon={ LockIcon } />
     </div>
   );
 }
